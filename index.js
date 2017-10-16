f782dfe5cf04c105f1914366237994eb50f0d993
Ashlie Witherspoon <ashlie0692@hotmail.com>
2017-10-16 23:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function happyHolidays() {
   return ("Happy holidays!");
 }
+<<<<<<< HEAD
 
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`);
@@ -13,3 +14,5 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return (`It\'s ${days} days until ${holiday}!`);
 }
+=======
+>>>>>>> b2f49f80db38292d32a5163293e9fe3f96a0aaab
